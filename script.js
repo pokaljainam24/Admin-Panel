5276281b8647c4747a8644dface934fdf2f6bbaf
pokaljainam24 <pokaljainam@gmail.com>
2024-04-17 09:16:36 +0530

    done

--- script.js
+++ script.js
@@ -53,7 +53,7 @@
 //   });
 
 
-// ---------------------------------- SIDEBAR HIDE&SHOW EFFECT ---------------------------------------------------------------- //
+// ---------------------------------- SIDEBAR HIDE&SHOW EFFECT ------------------------------------------------------------- //
 
 
 $("#content>div").hide();
@@ -213,123 +213,123 @@ var chart5 = document.getElementById("stackedbarchart");
 var chart6 = document.getElementById("radarchart");
 
 var myChart1 = new Chart(chart1, {
-    type: 'line',
-    data: {
-            labels: ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
-            datasets: [{
-                backgroundColor: "rgba(48, 164, 255, 0.2)",
-                borderColor: "rgba(48, 164, 255, 0.8)",
-                data: ['1150', '1160', '1160','1165', '1160', '1170','1175', '1165', '1170','1180', '1185', '1190'],
-                label: '',
-                fill: true
-            }]
-    },
-    options: {
-        responsive: true,
-        title: {display: false,text: 'Chart'},
-        legend: {position: 'top',display: false,},
-        tooltips: {mode: 'index',intersect: false,},
-        hover: {mode: 'nearest',intersect: true},
-        scales: {
-            xAxes: [{
-                display: true,
-                scaleLabel: {
-                    display: true,
-                    labelString: 'Months'
-                }
-            }],
-            yAxes: [{
-                display: true,
-                scaleLabel: {
-                    display: true,
-                    labelString: 'Sales volume'
-                }
-            }]
+  type: 'line',
+  data: {
+    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    datasets: [{
+      backgroundColor: "rgba(48, 164, 255, 0.2)",
+      borderColor: "rgba(48, 164, 255, 0.8)",
+      data: ['1150', '1160', '1160', '1165', '1160', '1170', '1175', '1165', '1170', '1180', '1185', '1190'],
+      label: '',
+      fill: true
+    }]
+  },
+  options: {
+    responsive: true,
+    title: { display: false, text: 'Chart' },
+    legend: { position: 'top', display: false, },
+    tooltips: { mode: 'index', intersect: false, },
+    hover: { mode: 'nearest', intersect: true },
+    scales: {
+      xAxes: [{
+        display: true,
+        scaleLabel: {
+          display: true,
+          labelString: 'Months'
         }
+      }],
+      yAxes: [{
+        display: true,
+        scaleLabel: {
+          display: true,
+          labelString: 'Sales volume'
+        }
+      }]
     }
+  }
 });
 
 
 // -----------------------------------------------------DOUGHNUT CHART----------------------------------------- //
 
 var myChart4 = new Chart(chart4, {
-    type: 'doughnut',
-    data: {
-        labels: ["Married", "Single", "Widowed", "Legaly Separated", "Anulled"],
-        datasets: [{
-            data: ["60", "45", "6", "3", "11"],
-            backgroundColor: ["#F44336","#2196F3","#795548","#6da252","#f39c12","#009688","#673AB7"],
-        }]
+  type: 'doughnut',
+  data: {
+    labels: ["Married", "Single", "Widowed", "Legaly Separated", "Anulled"],
+    datasets: [{
+      data: ["60", "45", "6", "3", "11"],
+      backgroundColor: ["#F44336", "#2196F3", "#795548", "#6da252", "#f39c12", "#009688", "#673AB7"],
+    }]
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: true,
+    legend: { display: true, fullWidth: true, position: 'right', },
+    tooltips: {
+      callbacks: {
+        label: function (tooltipItem, data) {
+          var allData = data.datasets[tooltipItem.datasetIndex].data;
+          var tooltipLabel = data.labels[tooltipItem.index];
+          var tooltipData = allData[tooltipItem.index];
+          var total = 0;
+          var label = tooltipLabel.split(" - ");
+          for (var i in allData) { total += allData[i]; }
+          var tooltipPercentage = Math.round((tooltipData / total) * 100);
+          return label[0] + ' (' + tooltipPercentage + '%)';
+        }
+      }
     },
-    options: {
-        responsive: true,
-        maintainAspectRatio: true,
-        legend: {display: true,fullWidth: true,position: 'right',},
-        tooltips: {
-            callbacks: {
-                label: function(tooltipItem, data) {
-                    var allData = data.datasets[tooltipItem.datasetIndex].data;
-                    var tooltipLabel = data.labels[tooltipItem.index];
-                    var tooltipData = allData[tooltipItem.index];
-                    var total = 0;
-                    var label = tooltipLabel.split(" - ");
-                    for (var i in allData) {total += allData[i];}
-                    var tooltipPercentage = Math.round((tooltipData / total) * 100);
-                    return label[0] + ' (' + tooltipPercentage + '%)';
-                }
-            }
-        },
-    }
+  }
 });
 
 // --------------------------------------------------BAR CHART-------------------------------------------- //
 
 var myChart5 = new Chart(chart5, {
-    type: 'bar',
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-            label: 'Income',
-            backgroundColor: "rgba(0, 150, 136, .5)",
-            borderColor: "rgb(0, 150, 136)",
-            borderWidth: 1,
-            data: ["20","30","40","50","60","70","80"],
-        }, {
-            label: 'Liabilites',
-            backgroundColor: "rgba(76, 175, 80, .5)",
-            borderColor: "rgba(76, 175, 80)",
-            borderWidth: 1,
-            data: ["2","5","15","25","35","25","15"],
-        }]
+  type: 'bar',
+  data: {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [{
+      label: 'Income',
+      backgroundColor: "rgba(0, 150, 136, .5)",
+      borderColor: "rgb(0, 150, 136)",
+      borderWidth: 1,
+      data: ["20", "30", "40", "50", "60", "70", "80"],
+    }, {
+      label: 'Liabilites',
+      backgroundColor: "rgba(76, 175, 80, .5)",
+      borderColor: "rgba(76, 175, 80)",
+      borderWidth: 1,
+      data: ["2", "5", "15", "25", "35", "25", "15"],
+    }]
+  },
+  options: {
+    responsive: true,
+    title: { display: false, text: 'Chart' },
+    legend: { position: 'top', display: true, },
+    tooltips: { mode: 'index', intersect: false, },
+    hover: { mode: 'nearest', intersect: true },
+    tooltips: {
+      mode: 'index',
+      intersect: false
     },
-    options: {
-        responsive: true,
-        title: {display: false,text: 'Chart'},
-        legend: {position: 'top',display: true,},
-        tooltips: {mode: 'index',intersect: false,},
-        hover: {mode: 'nearest',intersect: true},
-        tooltips: {
-            mode: 'index',
-            intersect: false
-        },
-        responsive: true,
-        scales: {
-            xAxes: [{
-                stacked: true,
-                scaleLabel: {
-                    display: true,
-                    labelString: 'Months'
-                }
-            }],
-            yAxes: [{
-                stacked: true,
-                scaleLabel: {
-                    display: true,
-                    labelString: 'Volume'
-                }
-            }]
+    responsive: true,
+    scales: {
+      xAxes: [{
+        stacked: true,
+        scaleLabel: {
+          display: true,
+          labelString: 'Months'
         }
+      }],
+      yAxes: [{
+        stacked: true,
+        scaleLabel: {
+          display: true,
+          labelString: 'Volume'
+        }
+      }]
     }
+  }
 });
 
 
@@ -337,15 +337,15 @@ var myChart5 = new Chart(chart5, {
 // COUNTER IN DASHBOARD CARD CONTENT ---------------------------------------------------------
 
 $("#counter").counter({
-  autoStart: true,          
-  duration: 1000,          
-  countFrom: 10,    
-  countTo: 6267,               
-  runOnce: false,    
-  placeholder: "6,267",     
-                      
-  easing: "easeOutCubic",     
-                              
+  autoStart: true,
+  duration: 1000,
+  countFrom: 10,
+  countTo: 6267,
+  runOnce: false,
+  placeholder: "6,267",
+
+  easing: "easeOutCubic",
+
   // onStart: function() {},     
   // onComplete: function() {},  
   // numberFormatter:            
@@ -355,32 +355,32 @@ $("#counter").counter({
 });
 
 $("#counter2").counter({
-  autoStart: true,          
-  duration: 1000,          
-  countFrom: 10,    
-  countTo: 180900,               
-  runOnce: false,    
-  placeholder: "$180,900",     
-                      
-  easing: "easeOutCubic",     
-                              
-  onStart: function() {},     
-  onComplete: function() {},  
-  numberFormatter:            
-    function(number) {
+  autoStart: true,
+  duration: 1000,
+  countFrom: 10,
+  countTo: 180900,
+  runOnce: false,
+  placeholder: "$180,900",
+
+  easing: "easeOutCubic",
+
+  onStart: function () { },
+  onComplete: function () { },
+  numberFormatter:
+    function (number) {
       return "$ " + number;
     }
 });
 
 $("#counter3").counter({
-  autoStart: true,          
-  duration: 1000,          
-  countFrom: 100,    
-  countTo: 28210,               
-  runOnce: false,    
-  placeholder: "28,210",     
-                      
-  easing: "easeOutCubic",     
-                              
-  
+  autoStart: true,
+  duration: 1000,
+  countFrom: 100,
+  countTo: 28210,
+  runOnce: false,
+  placeholder: "28,210",
+
+  easing: "easeOutCubic",
+
+
 });
