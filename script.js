185034190d75906307cfdee702e36676b3f02a16
pokaljainam24 <pokaljainam@gmail.com>
2024-04-18 05:55:05 -0400

    done

--- script.js
+++ script.js
@@ -105,6 +105,7 @@ var chart = new ApexCharts(document.querySelector("#chart2"), options);
 chart.render();
 
 
+
 // COUNTER IN DASHBOARD CARD CONTENT ---------------------------------------------------------
 
 $("#counter").counter({
@@ -178,3 +179,91 @@ var calendarInstance2 = new calendarJs(calendarElement, {
   manualEditingEnabled: true
   // All your options can be set here
 }); 
+
+
+// CHART-3 //
+
+var options = {
+  series: [
+  {
+    name: 'box',
+    type: 'boxPlot',
+    data: [
+      {
+        x: new Date('2017-01-01').getTime(),
+        y: [54, 66, 69, 75, 88]
+      },
+      {
+        x: new Date('2018-01-01').getTime(),
+        y: [43, 65, 69, 76, 81]
+      },
+      {
+        x: new Date('2019-01-01').getTime(),
+        y: [31, 39, 45, 51, 59]
+      },
+      {
+        x: new Date('2020-01-01').getTime(),
+        y: [39, 46, 55, 65, 71]
+      },
+      {
+        x: new Date('2021-01-01').getTime(),
+        y: [29, 31, 35, 39, 44]
+      }
+    ]
+  },
+  {
+    name: 'outliers',
+    type: 'scatter',
+    data: [
+      {
+        x: new Date('2017-01-01').getTime(),
+        y: 32
+      },
+      {
+        x: new Date('2018-01-01').getTime(),
+        y: 25
+      },
+      {
+        x: new Date('2019-01-01').getTime(),
+        y: 64
+      },
+      {
+        x: new Date('2020-01-01').getTime(),
+        y: 27
+      },
+      {
+        x: new Date('2020-01-01').getTime(),
+        y: 78
+      },
+      {
+        x: new Date('2021-01-01').getTime(),
+        y: 15
+      }
+    ]
+  }
+],
+  chart: {
+  type: 'boxPlot',
+  height: 700
+},
+colors: ['#008FFB', '#FEB019'],
+title: {
+  text: 'BoxPlot - Scatter Chart',
+  align: 'left'
+},
+xaxis: {
+  type: 'datetime',
+  tooltip: {
+    formatter: function(val) {
+      return new Date(val).getFullYear()
+    }
+  }
+},
+tooltip: {
+  shared: false,
+  intersect: true
+}
+};
+
+var chart = new ApexCharts(document.querySelector("#chart3"), options);
+chart.render();
